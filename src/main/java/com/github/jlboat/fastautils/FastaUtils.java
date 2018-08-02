fdc1a0fb6c68fe48e7249a077133ebf831acadef
Lucas Boatwright <lboat@ufl.edu>
2018-08-02 10:04:30 -0400

    Renamed files/paths to FastaUtils after deleting old project

--- src/main/java/com/github/jlboat/fastautils/FastaUtils.java
+++ src/main/java/com/github/jlboat/fastautils/FastaUtils.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package com.github.jlboat.fastautil;
+package com.github.jlboat.fastautils;
 
 import java.io.IOException;
 
