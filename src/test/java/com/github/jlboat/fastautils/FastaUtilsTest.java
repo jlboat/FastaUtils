4ddadf251337ce3940d6822e99690c1254fc9ef3
Lucas Boatwright <lboat@ufl.edu>
2018-08-03 16:30:13 -0400

    Minor edits

--- src/test/java/com/github/jlboat/fastautils/FastaUtilsTest.java
+++ src/test/java/com/github/jlboat/fastautils/FastaUtilsTest.java
@@ -51,7 +51,7 @@ public class FastaUtilsTest {
         int result = FastaUtils.getSeqCount(fasta);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        // fail("The test case is a prototype.");
     }
 
 //    /**
