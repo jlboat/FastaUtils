8df643a1f45661d62963f793d2485d1164f5fbb2
Lucas Boatwright <lboat@ufl.edu>
2018-08-03 15:06:10 -0400

    Removed main and added static methods

--- src/main/java/com/github/jlboat/fastautils/FastaUtils.java
+++ src/main/java/com/github/jlboat/fastautils/FastaUtils.java
@@ -23,25 +23,78 @@
  */
 package com.github.jlboat.fastautils;
 
-import java.io.IOException;
+import java.util.Arrays;
 
 /**
  *
  * @author lboat
  */
 public class FastaUtils {
-
-    /**
-     * @param args the command line arguments
-     * @throws java.io.IOException
-     */
-    public static void main(String[] args) throws IOException {
-        Fasta fasta = new Fasta(args[0], "DNA");
-        System.out.println(fasta.containsAmbiguous());
-        fasta.getValues().forEach((seq) -> {
-            System.out.println(((DNA)seq).reverseComplement());
-        });
-        fasta.toFile(args[1]);
+   
+    public static int[] getLengths(Fasta fasta){
+        Sequence[] seqs = fasta.getValues().toArray(new Sequence[0]);
+        int[] lengths = new int[seqs.length];
+        for (int i = 0; i < seqs.length; i++) {
+            lengths[i] = seqs[i].length();            
+        }
+        Arrays.sort(lengths);
+        return lengths;
     }
     
+    public static int nX(Fasta fasta, double x){
+        if ((x>100)||(x<0)){
+            System.err.printf("Invalid value for nX: %f:.2",x);
+            System.err.println("Must be between 0 and 100");
+            System.err.println("Typical values include N25, N50, N75");
+            return -1;
+        }
+        x = x/100;
+        int[] lengths = getLengths(fasta);
+        int[] cum_sum = baseCumulativeSum(fasta);
+        int total_base_count = cum_sum[lengths.length-1];
+        for (int i = 0; i < cum_sum.length; i++) {
+            if ((cum_sum[i]/total_base_count) > x){
+                return lengths[i];
+            }
+        }
+        // In the event the above code fails
+        return -1;
+    }
+    
+    public static int minLength(Fasta fasta){
+        int[] lengths = getLengths(fasta);
+        return lengths[0];
+    }
+    
+    public static int maxLength(Fasta fasta){
+        int[] lengths = getLengths(fasta);
+        return lengths[lengths.length-1];
+    }
+    
+    public static double medianLength(Fasta fasta){
+        int[] lengths = getLengths(fasta);
+        int index = lengths.length/2 + 1;
+        if(lengths.length % 2 == 1){ //odd num -- exact middle           
+            return lengths[index];
+        } else { // even number -- double
+            double median = (lengths[index-1] + lengths[index])/2;
+            return median;
+        }
+    }
+    
+    public static double meanLength(Fasta fasta){
+        int[] lengths = getLengths(fasta);
+        int total_length = baseCumulativeSum(fasta)[lengths.length-1];
+        return total_length/lengths.length;
+    }
+    
+    public static int[] baseCumulativeSum(Fasta fasta){
+        int[] lengths = getLengths(fasta);
+        int[] cum_sum = new int[lengths.length];
+        cum_sum[0] = lengths[0];
+        for (int i = 1; i < cum_sum.length; i++) {
+            cum_sum[i] = cum_sum[i-1] + lengths[i];
+        }
+        return cum_sum;
+    }
 }
